772ba93c889f04180c43493d9a7617a8a487710e
Erik Kronberg <erik.a.kronberg@gmail.com>
2016-06-18 15:25:06 +0200

    Connected configuration in GUI with code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 var app = angular.module('robot', []);
 app.controller('RobotController', function ($scope) {
   $scope.submit = function () {
-    $scope.result = followCommands({}, $scope.commands);
+    $scope.result = followCommands({
+      bounds: {
+        x: $scope.boundsX,
+        y: $scope.boundsY
+      },
+      position: {
+        x: $scope.startPositionX,
+        y: $scope.startPositionY
+      },
+      language: $scope.language
+    }, $scope.commands);
   };
+
+  $scope.boundsX = 5;
+  $scope.boundsY = 5;
+  $scope.startPositionX = 0;
+  $scope.startPositionY = 0;
+  $scope.language = 'english';
 });
